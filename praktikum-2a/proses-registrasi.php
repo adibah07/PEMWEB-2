e0a2ce7fadcc795402586acef40158a50c06306d
adibah07 <0110124040@student.nurulfikri.ac.id>
2025-03-13 16:17:46 +0700

    upload file 3

--- praktikum-2a/proses-registrasi.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$ar_prodi = ["SI"=>"Sistem Informasi","TI"=>"Teknik Informatika","BD"=>"Bisnis Digital"];
-$ar_skill = ["HTML"=>10,"CSS"=>10,"Java Script"=>20,"RWD Bootstrap"=>20,"PHP"=>30,"Java"=>50];
-$ar_domisili = ["Jakarta","Bogor","Depok","Tanggerang","Bekasi","Lainnya"];
-?>
